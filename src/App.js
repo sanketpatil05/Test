a2e3a59f61a0785525033ae4d307c199c3e08895
Sanket Patil <patilsanket209@gmail.com>
2022-08-03 23:11:33 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import "./App.css";
 // import data from db.json
+import data from "./db.json";
 
+import UserDetails from "./components/UserDetails";
+import { useState } from "react";
 function App() {
+
+  const [d, setD] =useState(data);
+ function handleSort(id){
  
+console.log(id);
+  let arr=d.sort((a, b) =>{
+      let fa = a.first_name,
+          fb = b.first_name;
+  
+      if (fa < fb) {
+          return -1*id ;
+      }
+      if (fa > fb) {
+          return 1* id;
+      }
+      return 0;
+ 
+  
+  })
+
+  setD([...arr]);
+
+ }
 
   return (
     <div className="App" data-testid = 'app'>
-      <button  data-testid = 'sort-asc-btn'>Sort by Asc</button>
-      <button  data-testid = 'sort-desc-btn'>Sort by Desc</button>
-      {/*  map through the users data and pass props to the Userdetails component */}
+      <button  data-testid = 'sort-asc-btn' onClick={()=>{
+         handleSort(1)
+      }}>Sort by Asc</button>
+      <button  data-testid = 'sort-desc-btn'
+       onClick={()=>{
+        handleSort(-1);
+       }}>Sort by Desc</button>
+      {/*  map through the users data and pass props to the Userdetails component */
+      d.map((el) => (
+
+         <UserDetails key={el.id}  user={el}/>
+      ))
+      
+      }
     </div>
   );
 }
--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,28 +1,43 @@
 // import module.css here;
-const UserDetails = (props) => {
+//import UserDetails from './userDetails.module.css'
+
+import "../index.css";
+
+const UserDetails = ({user}) => {
+
+  
   return (
     <>
-      <div data-testid="user-container">
-        {/* user image */}
+      <div data-testid="user-container" style={{display: 'flex'}}>
+
         <div>
-          <div>
-            <h3 data-testid="user-fname"></h3>
-            <h3 data-testid="user-lname"></h3>
+        <img src={user.avatar} alt=""  height="60px" width="60px"/>
+        </div>
+        
+        <div>
+          <div style={{display: 'flex'}}>
+            <h3 data-testid="user-fname">{user.first_name}</h3>
+            <h3 data-testid="user-lname">{user.last_name}</h3>
           </div>
           <div>
-            <p data-testid="user-address"></p>
+            <p data-testid="user-address">{user.address}</p>
           </div>
         </div>
-        <div>
-          <h3 data-testid="user-karma"></h3>
+
+         <div className="logo">
+         <div>
+          <h3 data-testid="user-karma" className="font">{user.karma}</h3>
         </div>
         <div>
-          <h3 data-testid="user-followers"></h3>
+          <h3 data-testid="user-followers" className="font">{user.followers}K</h3>
         </div>
         <div>
-          <h3 data-testid="user-posts"></h3>
+          <h3 data-testid="user-posts" className="font">{user.posts}</h3>
         </div>
-        <button data-testid="follow-btn"></button>
+
+         </div>
+        
+        <button data-testid="follow-btn"  >{user.is_following ? "Unfollow": "Follow"}</button>
       </div>
     </>
   );
